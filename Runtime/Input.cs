a3f73ddc3a804f072ed2788556490c43b5b14c0c
Cuku <xhikab@gmail.com>
2026-05-28 11:39:03 +0200

    Visualize entity store

--- Runtime/Input.cs
+++ Runtime/Input.cs
@@ -1,5 +1,6 @@
 using Friflo.Engine.ECS;
 using Friflo.Engine.Unity;
+using FrifloECS.Unity.EntityVisualize;
 using UnityEngine;
 
 namespace ECS.Input {
@@ -8,5 +9,13 @@ namespace ECS.Input {
 		public static EntityStore Store;
 
 		void Awake() => Store = GetComponent<ECSStore>().EntityStore;
-	}
+
+#if UNITY_EDITOR
+        [SerializeField] bool visualize = true;
+
+        private void Start() {
+            EntityVisualizer.Register(name, Store);
+        }
+#endif
+    }
 }
